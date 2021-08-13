98690f2533b98809f3644c864d74594aa3e6f6ef
Matt <attackdebris@gmail.com>
2021-08-13 11:22:19 +0100

    Improve usage

--- append_numbers_to_word.py
+++ append_numbers_to_word.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
-word  = 'NewIntelligenceCorpUser'
-for count in range(9876, 9999):
+import sys
+
+# Simple script to append numbers after a keyword entered on the cmd line
+
+instructions =  "append_numbers_to_word.py - a script to simply append numbers to a word\n" +\
+                "Usage: append_numbers_to_word.py [word]"
+
+if len(sys.argv) <2 or len(sys.argv) != 2 or sys.argv[1] == "-h" or sys.argv[1] == "--h" or sys.argv[1] == "-help" or sys.argv[1] == "--help":
+        print (instructions)
+        sys.exit()
+
+word = sys.argv[1]
+
+for count in range(1, 1000): # Set the number range here i.e. 1 - 999 
 	print(word + str(count))
